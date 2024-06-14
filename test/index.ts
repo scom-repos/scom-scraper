e4812c99de32a3d42dfdb8c479a2f99711e6be01
johnnycheukijs <johnnycheuk@instajs.com>
2024-06-14 18:26:01 +0800

    Update scraper method

--- test/index.ts
+++ test/index.ts
@@ -3,5 +3,5 @@ import config from "./data/config";
 (async() => {
     const scraperManager = new ScraperManager(config);
     const tweets = await scraperManager.scrapTweetsByUsername('openswapdex');
-    console.log(`Total tweets scraped: ${tweets.length}`);
+    console.log(`Total tweets scraped: ${tweets?.length}`);
 })();
