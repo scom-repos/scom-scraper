0e5eac0ff7ec7e51fdbf73576bc979ead21981ec
johnnycheuk <johnnycheuk2649@gmail.com>
2024-08-22 15:56:22 +0800

    Update built folder

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,72 @@
 define("@scom/scom-scraper", ["require", "exports", "puppeteer"], function (require, exports, puppeteer_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.Page = exports.Browser = void 0;
-    Object.defineProperty(exports, "Page", { enumerable: true, get: function () { return puppeteer_1.Page; } });
-    Object.defineProperty(exports, "Browser", { enumerable: true, get: function () { return puppeteer_1.Browser; } });
     class ScraperManager {
         constructor() { }
+        async init() {
+            this.browser = await puppeteer_1.default.launch({
+                headless: true,
+                args: ["--no-sandbox"],
+                defaultViewport: {
+                    width: 1920,
+                    height: 1080
+                }
+            });
+            this.page = await this.browser.newPage();
+            this.keyboard = new ScraperKeyboard(this.page);
+        }
+        // Mouse interaction
+        async click(selector) {
+            return this.page.click(selector);
+        }
+        // Keyboard interaction
+        async type(selector, value, timeout = 30000, delay = 0) {
+            await this.waitForSelector(selector, timeout);
+            return this.page.type(selector, value, { delay });
+        }
+        // Page Behavior
+        async goTo(url, timeout = 30000, waitUntil = 'domcontentloaded') {
+            return this.page.goto(url, { timeout, waitUntil });
+        }
+        async waitForNavigation(timeout = 30000, waitUntil = 'domcontentloaded') {
+            return this.page.waitForNavigation({ timeout, waitUntil });
+        }
+        async waitForSelector(selector, timeout = 30000) {
+            return this.page.waitForSelector(selector, { timeout });
+        }
+        async scrollToBottom(delay = 0) {
+            if (delay > 0)
+                await this.sleep(delay);
+            await this.page.evaluate(() => {
+                window.scrollTo(0, document.body.scrollHeight);
+            });
+        }
+        // Page networking
+        async waitForRequest(urlOrPredict) {
+            return this.page.waitForRequest(urlOrPredict);
+        }
+        async waitForResponse(urlOrPredict) {
+            return this.page.waitForResponse(urlOrPredict);
+        }
+        removeAllListeners() {
+            return this.page.removeAllListeners();
+        }
+        on(event, callback) {
+            return this.page.on(event, callback);
+        }
+        // Page storage
+        async getCookies() {
+            return this.page.cookies();
+        }
+        async setCookies(...cookies) {
+            return this.page.setCookie(...cookies);
+        }
+        // Scraper Manager Lifecycle
+        async destroy() {
+            await this.page.close();
+            await this.browser.close();
+        }
+        // Deprecated
         async getBrowserAndPage() {
             const browser = await puppeteer_1.default.launch({
                 headless: true,
@@ -30,6 +91,31 @@ define("@scom/scom-scraper", ["require", "exports", "puppeteer"], function (requ
                 return null;
             }
         }
+        async sleep(time) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve();
+                }, time);
+            });
+        }
     }
     exports.default = ScraperManager;
+    class ScraperKeyboard {
+        constructor(page) { this.page = page; }
+        async up(key) {
+            return this.page.keyboard.up(key);
+        }
+        async down(key) {
+            return this.page.keyboard.down(key);
+        }
+        async press(key, delay = 0) {
+            return this.page.keyboard.press(key, { delay });
+        }
+        async type(text) {
+            return this.page.keyboard.type(text);
+        }
+        async sendCharacter(char) {
+            return this.page.keyboard.sendCharacter(char);
+        }
+    }
 });
--- pluginTypes/index.d.ts
+++ pluginTypes/index.d.ts
@@ -1,12 +1,39 @@
 /// <amd-module name="@scom/scom-scraper" />
 declare module "@scom/scom-scraper" {
     import { Page, Browser } from "puppeteer";
+    import { ICookie, ICookieParam, IKeyInput, IPageEvent, IPageLifeCycle } from "./types";
     export default class ScraperManager {
+        private browser;
+        private page;
+        keyboard: ScraperKeyboard;
         constructor();
+        init(): Promise<void>;
+        click(selector: string): Promise<void>;
+        type(selector: string, value: string, timeout?: number, delay?: number): Promise<void>;
+        goTo(url: string, timeout?: number, waitUntil?: IPageLifeCycle | IPageLifeCycle[]): Promise<import("puppeteer").HTTPResponse>;
+        waitForNavigation(timeout?: number, waitUntil?: IPageLifeCycle | IPageLifeCycle[]): Promise<import("puppeteer").HTTPResponse>;
+        waitForSelector(selector: string, timeout?: number): Promise<import("puppeteer").ElementHandle<Element>>;
+        scrollToBottom(delay?: number): Promise<void>;
+        waitForRequest(urlOrPredict: string | ((response: any) => boolean)): Promise<import("puppeteer").HTTPRequest>;
+        waitForResponse(urlOrPredict: string | ((response: any) => boolean)): Promise<import("puppeteer").HTTPResponse>;
+        removeAllListeners(): Page;
+        on(event: IPageEvent, callback: () => any): Page;
+        getCookies(): Promise<ICookie[]>;
+        setCookies(...cookies: ICookieParam[]): Promise<void>;
+        destroy(): Promise<void>;
         getBrowserAndPage(): Promise<{
             browser: Browser;
             page: Page;
         }>;
+        private sleep;
     }
-    export { Browser, Page };
+    class ScraperKeyboard {
+        private page;
+        constructor(page: Page);
+        up(key: IKeyInput): Promise<void>;
+        down(key: IKeyInput): Promise<void>;
+        press(key: IKeyInput, delay?: number): Promise<void>;
+        type(text: string): Promise<void>;
+        sendCharacter(char: string): Promise<void>;
+    }
 }
